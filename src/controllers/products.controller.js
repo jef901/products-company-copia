c129ce50673daca798f6da19d62cf4d7e4ae8d30
Jose Freire <jef901@gmail.com>
2021-11-16 11:34:51 -0300

    segundo commit

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -19,10 +19,28 @@ export const createProduct = async (req, res) => {
    const products = await Product.find();
    return res.json(products);
     }
- export const getProductById = (req, res) => {
+ export const  getProductById = async (req, res) => {
+   const { productId } = req.params;
+
+   const product = await Product.findById(productId);
+   res.status(200).json(product);
     }   
- export const updateProductById = (req, res) => {
+ export const updateProductById = async (req, res) => {
+   const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedProduct);
   }
  
-  export const deleteProductById = (req, res) => {
+  export const deleteProductById = async (req, res) => {
+    const { productId } = req.params;
+
+    await Product.findByIdAndDelete(productId);
+  
+    // code 200 is ok too
+    res.status(200).json();
 } 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,7 +7,7 @@ import * as productsCtrl from "../controllers/products.controller";
 router.post("/",productsCtrl.createProduct)
 router.get("/",productsCtrl.getProducts)
 router.get("/:productId",productsCtrl.getProductById)
-router.put(":productId",productsCtrl.updateProductById)
+router.put("/:productId",productsCtrl.updateProductById)
 router.delete("/:productId",productsCtrl.deleteProductById)
 
 
